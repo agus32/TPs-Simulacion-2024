1e69c5d2620535e038a8f5a01dffc9e15034cfa2
agus32 <agusluzzini@gmail.com>
2024-05-08 20:51:48 -0300

    compactado simulador apuesta y añadido estrategia a los parametros

--- TP2/simulador_apuesta.py
+++ TP2/simulador_apuesta.py
@@ -5,19 +5,30 @@ from apuestas import simular_martingala, simular_dalambert, simular_fibonacci, s
 
 #En este archivo ira la entrada de parametros y la visualizacion de los resultados
 
-if len(sys.argv) != 9 or sys.argv[1] != "-c" or sys.argv[3] != "-n" or sys.argv[5] != "-e" or sys.argv[7] != "-d" or int(sys.argv[6]) not in range(37) or int(sys.argv[4]) > 10:
-    print("Entrada incorrecta: python simulacion_ruleta.py -c <num_tiradas> -n <num_corridas -e <num_elegido> -d <dinero_inicial>")
+if len(sys.argv) != 9 or sys.argv[1] != "-c" or sys.argv[3] != "-n" or sys.argv[5] != "-s" or sys.argv[7] != "-d" or int(sys.argv[4]) > 10:
+    print("Entrada incorrecta: python simulacion_ruleta.py -c <num_tiradas> -n <num_corridas> -s <estrategia: m/d/f/o> -d <dinero_inicial>")
     sys.exit(1)
 
 num_tiradas = int(sys.argv[2])
 num_corridas = int(sys.argv[4])
-valor_elegido = int(sys.argv[6])
+estrategia = sys.argv[6]
 dinero_inicial = int(sys.argv[8])
 
 
 
-# MARTINGALA
-tiradas_martingala, dinero_historico_martingala = simular_martingala(num_tiradas,num_corridas, dinero_inicial)
+if(estrategia == "m"):
+    tiradas, dinero_historico = simular_martingala(num_tiradas,num_corridas, dinero_inicial)
+elif(estrategia == "d"):
+    tiradas, dinero_historico = simular_dalambert(num_tiradas,num_corridas, dinero_inicial)
+elif(estrategia == "f"):
+    tiradas, dinero_historico = simular_fibonacci(num_tiradas,num_corridas, dinero_inicial)
+elif(estrategia == "o"):
+    tiradas, dinero_historico = simular_paroli(num_tiradas,num_corridas, dinero_inicial)
+else:
+    print("Estrategia no valida")
+    sys.exit(1)
+
+
 plt.figure(figsize=(13, 6))
 plt.xlabel('n (número de tiradas)')
 plt.ylabel('cc (cantidad de capital)')
@@ -25,54 +36,7 @@ plt.title('Evolución del flujo de caja vs número de tiradas')
 
 for i in range(num_corridas):
     color = np.random.rand(3,)
-    plt.plot(tiradas_martingala[i], dinero_historico_martingala[i], color=color, label=f'Corrida {i + 1}')
-
-plt.axhline(dinero_inicial, color='red', linestyle='--', label='Flujo de caja inicial')
-plt.legend()
-plt.show()
-
-# D'ALAMBERT
-tiradas_dalambert, dinero_historico_dalambert = simular_dalambert(num_tiradas,num_corridas, dinero_inicial)
-plt.figure(figsize=(13, 6))
-plt.xlabel('n (número de tiradas)')
-plt.ylabel('cc (cantidad de capital)')
-plt.title('Evolución del flujo de caja vs número de tiradas')
-
-for i in range(num_corridas):
-    color = np.random.rand(3,)
-    plt.plot(tiradas_dalambert[i], dinero_historico_dalambert[i], color=color, label=f'Corrida {i + 1}')
-
-plt.axhline(dinero_inicial, color='red', linestyle='--', label='Flujo de caja inicial')
-plt.legend()
-plt.show()
-
-
-# FIBONACCI
-tiradas_fibonacci, dinero_historico_fibonacci = simular_fibonacci(num_tiradas,num_corridas, dinero_inicial)
-plt.figure(figsize=(13, 6))
-plt.xlabel('n (número de tiradas)')
-plt.ylabel('cc (cantidad de capital)')
-plt.title('Evolución del flujo de caja vs número de tiradas')
-
-for i in range(num_corridas):
-    color = np.random.rand(3,)
-    plt.plot(tiradas_fibonacci[i], dinero_historico_fibonacci[i], color=color, label=f'Corrida {i + 1}')
-
-plt.axhline(dinero_inicial, color='red', linestyle='--', label='Flujo de caja inicial')
-plt.legend()
-plt.show()
-
-# PAROLI
-tiradas_paroli, dinero_historico_paroli = simular_paroli(num_tiradas,num_corridas, dinero_inicial)
-plt.figure(figsize=(13, 6))
-plt.xlabel('n (número de tiradas)')
-plt.ylabel('cc (cantidad de capital)')
-plt.title('Evolución del flujo de caja vs número de tiradas')
-
-
-for i in range(num_corridas):
-    color = np.random.rand(3,)
-    plt.plot(tiradas_paroli[i], dinero_historico_paroli[i], color=color, label=f'Corrida {i + 1}')
+    plt.plot(tiradas[i], dinero_historico[i], color=color, label=f'Corrida {i + 1}')
 
 plt.axhline(dinero_inicial, color='red', linestyle='--', label='Flujo de caja inicial')
 plt.legend()
